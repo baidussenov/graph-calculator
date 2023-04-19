aad50987df07639989e7ed9f6128c3f74d72a924
Alikhan Baidussenov <a.baidusenov1@gmail.com>
2023-04-19 15:10:17 +0600

    Added decimal to 7seg display converter

---
+++ dec_to_7seg.v
@@ -0,0 +1,21 @@
+module dec_to_7seg (out, in);
+
+output [6:0] out;
+input in;
+
+reg [6:0] out;
+
+always @(*)
+	case (in)
+		0: out = 7'b1000000;
+		1: out = 7'b1111001;
+		2: out = 7'b0100100;
+		3: out = 7'b0110000;
+		4: out = 7'b0011001;
+		5: out = 7'b0010010;
+		6: out = 7'b0000010;
+		7: out = 7'b1111000;
+		8: out = 7'b0000000;
+		9: out = 7'b0011000;
+	endcase
+endmodule
