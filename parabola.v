a133f5f9b69da8695a9f2f34fe9a937489c77fb0
OneSanchMan <sanzhar.mazhit@gmail.com>
2023-04-20 00:02:32 +0600

    Basic input interface

--- parabola.v
+++ parabola.v
@@ -1,96 +1,5 @@
-//module parabola(LEDR, CLOCK_50);
-//output [8:0] LEDR;
-//input CLOCK_50;
-//wire clk_1Hz;
-//clockDivider dataset(.clk(CLOCK_50),
-//.clk_div(clk_1Hz));
-//assign LEDR[0]=clk_1Hz;
-//endmodule
-//
-//module draw(sw, sw1, clk, reset, MTL2_DCLK, MTL2_R, MTL2_G, MTL2_B, MTL2_HSD, 
-//MTL2_VSD); 
-//input clk; 
-//input reset; 
-//input sw; 
-//input sw1; 
-//output MTL2_DCLK; 
-//output [7:0] MTL2_R; 
-//output [7:0] MTL2_G; 
-//output [7:0] MTL2_B; 
-//output MTL2_HSD; 
-//output MTL2_VSD; 
-//reg [7:0] red, green, blue; 
-//wire res=~reset; 
-//wire display_on; 
-//wire [11:0] hpos; 
-//wire [11:0] vpos; 
-//reg clk25 = 0; 
-//always @(posedge clk) 
-// clk25<=~clk25; 
-//hvsync test( 
-// .clk(clk25), 
-// .reset(0), 
-// .data_enable(display_on), 
-// .hsync(MTL2_HSD), 
-// .vsync(MTL2_VSD), 
-// .hpos(hpos), 
-// .vpos(vpos) 
-//);
-//
-//reg [3:0] oREG_TOUCH_COUNT;
-//i2c_touch_config test2(
-//	.oREG_TOUCH_COUNT(oREG_TOUCH_COUNT)
-//);
-//reg [11:0] posH; 
-//initial posH = 40; 
-//reg [11:0] posV; 
-//initial posV = 60; 
-//reg [11:0] shiftH; 
-//initial shiftH = 3; 
-//reg [11:0] shiftV; 
-//initial shiftV = 2;
-//
-//reg [11:0] x;
-//reg [11:0] y;
-//
-//always @(posedge clk25) 
-//begin
-//
-// x <= hpos - 400;
-// y <= vpos - 240;
-//
-// red <= 8'd0; 
-// green <= 8'd0; 
-// blue <= 8'd0; 
-//// if (hpos >= posH && hpos <= posH + 20 && vpos >= posV && vpos <= posV + 20 && oREG_TOUCH_COUNT > 0) begin 
-////  red <= 8'hcc; 
-////  green <= 8'h00; 
-////  blue <= 8'h00; 
-//// end
-//// else if (hpos >= posH && hpos <= posH + 20 && vpos >= posV && vpos <= posV + 20) begin
-////	red <= 8'h00; 
-////	green <= 8'hcc; 
-////	blue <= 8'h00; 
-//// end
-// 
-// if (y == x * x) begin
-//	red <= 8'h00; 
-//	green <= 8'hcc; 
-//	blue <= 8'h00;
-// end
-// 
-//end 
-//always @(negedge MTL2_VSD) 
-//begin 
-//
-//end 
-//assign MTL2_DCLK=clk25; 
-//assign MTL2_R=red; 
-//assign MTL2_G=green; 
-//assign MTL2_B=blue; 
-//endmodule
-module parabola(GPIO_0, clk, reset, MTL2_DCLK, MTL2_R, MTL2_G, MTL2_B, MTL2_HSD,
-MTL2_VSD);
+module parabola(GPIO_0, clk, reset, MTL2_DCLK, MTL2_R, MTL2_G, MTL2_B, MTL2_HSD, MTL2_VSD);
+
 input [35:0] GPIO_0;
 input clk;
 input reset;
@@ -140,22 +49,29 @@ generate
 );
    end
 endgenerate
+integer shiftX = 400;
+integer shiftY = 240;
+
+integer degree = 0;
+integer curCoef = 0;
+integer sign = 1;
+
+integer iter;
 
 always @(posedge clk25) begin
+	x <= hpos - shiftX;
+	y <= shiftY - vpos;
 	if (pressed[1]) begin
 		graph = ~graph;
 	end
 	if (graph) begin
-		x <= hpos - 400;
-		y <= 240 - vpos;
-		if (x === 0 || y === 0) begin
+		if (pressed[2])
+			shiftX = shiftX + 10;
+		if (pressed[3])
+			shiftX = shiftX - 10;	
+		if ((x === 0 || y === 0) && display_on) begin
 			red <= 8'h00;
 			green <= 8'h00;
-			blue <= 8'h00;
-		end
-		else if ((((x+1)*(x+1) >= y && y >= x*x)||((x+1)*(x+1)<= y && y <= x*x)) && y >= -400 && y <= 400) begin
-			red <= 8'h00; 
-			green <= 8'hcc; 
 			blue <= 8'h00;
 		end
 		else begin
@@ -165,13 +81,11 @@ always @(posedge clk25) begin
 		end
 	end
 	else begin
-		if (pressed[0]) begin
-			t <= t + 50;
+		if (pressed[2] && curCoef > 0) begin
+			curCoef <= curCoef - 1;
 		end
-		if(hpos<=30 && vpos<=50 && display_on) begin
-			red <= 8'h00;
-			green <= 8'h00;
-			blue <= 8'h00;
+		if (pressed[3] && curCoef < 4) begin
+			curCoef <= curCoef + 1;
 		end
 
 		else if (hpos < t) begin
@@ -183,6 +97,29 @@ always @(posedge clk25) begin
 			red <= 8'hcc;
 			green <= 8'hcc;
 			blue <= 8'hcc;
+		end
+		for (iter = 0; iter < 5; iter=iter+1) begin
+			if(hpos >= 20 + iter*30 && vpos >= 20 &&
+				40 + iter*30 >= hpos && 50 >= vpos &&display_on) begin
+				red <= 8'h00;
+				green <= 8'h00;
+				blue <= 8'h00;
+			end
+		end
+		if (pressed[0])
+			sign <= sign * (-1);
+		if(hpos >= 20 + curCoef*30 && vpos >= 20 &&
+			40 + curCoef*30 >= hpos && 50 >= vpos &&display_on) begin
+			if (sign > 0) begin
+				red <= 8'hcc;
+				green <= 8'h00;
+				blue <= 8'h00;
+			end
+			else if (sign < 0)begin
+				red <= 8'h00;
+				green <= 8'h00;
+				blue <= 8'hcc;
+			end
 		end
 	end
 end
